c066f17c5decc6a82d806c69c359d75a083d64cb
Ruben Vazquez <rubvaz1999@gmail.com>
2022-08-08 14:08:24 -0400

    Agregar repo

---
+++ script.js
@@ -0,0 +1,15 @@
+const h1= document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#Calculo1');
+const input2 = document.querySelector('#Calculo2');
+const btn = document.querySelector('#btnCalcular');
+const presult = document.querySelector('#result');
+
+form.addEventListener('submit',sumarInputValues)
+
+function sumarInputValues(event) {
+    console.log({event});
+    event.preventDefault();  
+    const sumaInputs = input1.value + input2.value;
+    presult.innerText = "Resultado: " + sumaInputs; 
+}
